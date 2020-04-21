adb71bfe6d3b6d1947448f44213022324c637b69
Colin Keilbach <colin.keilbach@mymail.champlain.edu>
2020-04-21 17:55:12 -0400

    Created Menu -Colin Keilbach

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -3,5 +3,19 @@
 
 int main()
 {
-	launchTicTacToe();
+	int input = -1;
+
+	printf(" Pick a Game to play!!!\n");
+	printf(" 1. Checkers\n");
+	printf(" 2. TicTacToe\n");
+	printf("Input: ");
+	scanf("%d", &input);
+
+
+	switch (input) {
+	case 1:
+		launchCheckers();
+	case 2:
+		launchTicTacToe();
+	}
 }
